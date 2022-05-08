efc03d6bea34d848037eebe76b3a4ac10541b963
Achref <medachrefg@gmail.com>
2022-05-09 00:33:13 +0100

    refreshing some concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+// 	return React.createElement(
+// 		'div',
+// 		{ title: 'div' },
+// 		React.createElement('h2', { title: 'a new h2' }, 'this is amazing')
+// 	);
+// }
+
+class App extends React.Component {
+	render() {
+		return React.createElement(
+			'div',
+			{ title: 'div' },
+			React.createElement('h2', { title: 'a new h2' }, 'this is amazing')
+		);
+	}
 }
 
 export default App;
