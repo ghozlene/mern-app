0fbab075f7bd0085ab4850e3b51b4a624a21e4af
Achref <medachrefg@gmail.com>
2022-05-11 11:46:07 +0100

    Working with multiple components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.css';
-
-// function App() {
-// 	return React.createElement(
-// 		'div',
-// 		{ title: 'div' },
-// 		React.createElement('h2', { title: 'a new h2' }, 'this is amazing')
-// 	);
-// }
-
-class App extends React.Component {
-	render() {
-		return React.createElement(
-			'div',
-			{ title: 'div' },
-			React.createElement('h2', { title: 'a new h2' }, 'this is amazing')
-		);
-	}
+import GoalsList from './components/GoalsList';
+function App() {
+	return (
+		<div className='course-goals'>
+			<h2>Course Goals</h2>
+			<GoalsList />
+		</div>
+	);
 }
+// class App extends React.Component {
+// 	render() {
+// 		return React.createElement(
+// 			'div',
+// 			{ title: 'div' },
+// 			React.createElement('h2', { title: 'a new h2' }, 'this is amazing')
+// 		);
+// 	}
+// }
 
 export default App;
---
+++ src/components/GoalsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GoalsList = () => {
+	return (
+		<ul className='goal-list'>
+			<li>finish the course</li>
+			<li>learning more</li>
+			<li>this the last course</li>
+		</ul>
+	);
+};
+
+export default GoalsList;
