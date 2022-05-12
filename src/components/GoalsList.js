221cf808f79480e86f3cad658193ecd5a4a977b9
Achref <medachrefg@gmail.com>
2022-05-12 12:47:18 +0100

    rendring list of data

--- src/components/GoalsList.js
+++ src/components/GoalsList.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './GoalsList.css';
 const GoalsList = ({ goals }) => {
 	console.log(goals);
-	return <ul className='goal-list'></ul>;
+	return (
+		<ul className='goal-list'>
+			{goals.map((item) => {
+				return <li key={item.id}>{item.text}</li>;
+			})}
+		</ul>
+	);
 };
 
 export default GoalsList;
