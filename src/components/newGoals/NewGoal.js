7af9d809806a4790975b8506bb2722f1b8190743
Achref <medachrefg@gmail.com>
2022-05-12 14:19:06 +0100

    fetching user input (two way binding

--- src/components/newGoals/NewGoal.js
+++ src/components/newGoals/NewGoal.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewGoal.css';
 const NewGoal = ({ onAddGoal }) => {
+	const [entredText, setEntredText] = useState('');
+
 	const addGoalHandler = (event) => {
 		event.preventDefault();
-		const newGoal = { id: Math.random(), text: 'my newgoal' };
+		const newGoal = { id: Math.random(), text: entredText };
 
 		onAddGoal(newGoal);
+		setEntredText('');
+	};
+
+	const inputHandler = (event) => {
+		setEntredText(event.target.value);
 	};
 
 	return (
 		<form className='new-goal' onSubmit={addGoalHandler}>
-			<input type='text' />
+			<input type='text' value={entredText} onChange={inputHandler} />
 			<button type='submit'>add goal</button>
 		</form>
 	);
